f427cd3b70b55f68ee39eb4f09aa85390fd01411
Robin <booksofcodeandpsychic@gmail.com>
2025-04-27 10:08:36 -0400

    Can now set html filepath from within program. Automated text positioning(manual input of html line number is no longer necessary) -Pre-Alpha 1.1

--- post.cpp
+++ post.cpp
@@ -3,31 +3,80 @@
 #include <string>
 #include <vector>
 #include <limits>
-
 using namespace std;
+
+int p(ofstream& indexWrite, string& input) {
+     bool writing = true;
+     cout << "Text: ";
+     getline(cin, input);
+     indexWrite << "\n<p>" << input << "<br>" <<  endl;
+     while (writing == true) {
+           cout << "Text: ";
+           getline(cin, input);
+           if (input == "\\") {
+           indexWrite << "</p>\n";
+           writing = false;
+           }
+      	     else {
+            	 indexWrite << input << "<br>\n";
+             }
+     }
+     return 1;
+}
+
+int post(ofstream& indexWrite, string& input) {
+        while (true) {
+                cout << "Post contents: ";
+                getline(cin, input);
+                if (input == "image") {
+                        cout << "Image title: ";
+                        getline(cin, input);
+                        indexWrite << "\n<img src=\"Images/4_2025/" << input << "\"></img>";
+                }
+		
+                else if (input == "p") {
+			while(true) {
+				if(p(indexWrite, input) == 1)
+				{
+					break;
+				}
+			}
+                }
+                else if (input == "done") {
+                        indexWrite << "</div>\n";
+			indexWrite << "\n<!-- ENDOFPOST -->" << endl;
+                        break;
+                }
+        }
+	return 0;
+}
 
 int main() {
 	ifstream index;
-	int indexLine = 0;
-	int targetLine;
-	string indexString;
-	string targetLineString;
-	string title;
-	string input;
-	vector<string> indexLines;
-	vector<string> otherLines;
+        int indexLine = 0;
+        int targetLine;
+        string path;
+        string indexString;
+        string targetLineString;
+        string title;
+        string input;
+        vector<string> indexLines;
+        vector<string> otherLines;
 
-	cout << "Target Line: ";
-	cin >> targetLineString;
-	targetLine = stoi(targetLineString);
-	index.open("bruce835.github.io/index.html");
+	cout << "Path to file(includes filename): ";
+	getline(cin, path);
+	index.open(path);
 
 	cout << "\nTitle: ";
 	cin >> title;
 
-	while (indexLine < targetLine) {
+	while (!index.eof()) {
 		indexLine++;
 		getline(index, indexString);
+		if (indexString == "<!-- ENDOFPOST -->") {
+			targetLine = indexLine;
+			break;
+		}
 		indexLines.push_back(indexString);
 	}
 
@@ -38,7 +87,7 @@ int main() {
 		otherLines.push_back(indexString);
 	}
 	
-	ofstream indexWrite("index.html", ios::trunc);
+	ofstream indexWrite(path, ios::trunc);
 	for (const string& currentLine : indexLines)
 	{
 		indexWrite << currentLine << endl;
@@ -48,42 +97,15 @@ int main() {
 	indexWrite << "\n<h3>" << title << "</h3>";
 	
 	 cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	 bool writing = false;
 
-	while (true) {
-		cout << "Post contents: ";
-		getline(cin, input);
-		if (input == "image") {
-			cout << "Image title: ";
-		        getline(cin, input);
-			indexWrite << "\n<img src=\"Images/4_2025/" << input << "\"></img>";
-		}
-		else if (input == "p") {
-			cout << "Text: ";
-			getline(cin, input);
-			indexWrite << "\n<p>" << input << "<br>" <<  endl;
-			writing = true;
-			while (writing == true) {
-				cout << "Text: ";
-				getline(cin, input);
-				if (input == "\\") {
-					indexWrite << "</p>\n";
-					writing = false;
-				}
-				else {
-					indexWrite << input << "<br>\n";
-				}
+	 while(true) {
+		 if (post(indexWrite, input) == 0) {
+			for (const string& currentLine : otherLines) {
+				indexWrite << currentLine << endl;
 			}
+		break;
 		}
-		else if (input == "done") {
-			indexWrite << "</div>\n";
-			break;
-		}
-	}
-
-		for (const string& currentLine : otherLines) {
-			indexWrite << currentLine << endl;
-		}
+	 }
 			
 	return 0;
 }
